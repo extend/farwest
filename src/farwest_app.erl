043cca2cfd04eb68df24022de1d665eb6fa00b4b
Loïc Hoguin <essen@dev-extend.eu>
2012-06-23 14:44:28 +0200

    Add application and listener startup

---
+++ src/farwest_app.erl
@@ -0,0 +1,43 @@
+%% Copyright (c) 2012, Loïc Hoguin <essen@ninenines.eu>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+%% @private
+-module(farwest_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+
+%% The priv_dir environment setting is mandatory.
+start(_, _) ->
+	PrivDir = case application:get_env(farwest, priv_dir) of
+		{ok, PD} -> PD
+	end,
+	{ok, Dispatch} = file:consult(PrivDir ++ "/dispatch.conf"),
+	Port = case application:get_env(farwest, port) of
+		{ok, P} -> P;
+		undefined -> 8080
+	end,
+	{ok, _} = cowboy:start_listener(farwest, 100,
+		cowboy_tcp_transport, [{port, Port}],
+		cowboy_http_protocol, [{dispatch, Dispatch}]
+	),
+	lager:info("Farwest listening on port ~p~n", [Port]),
+	farwest_sup:start_link().
+
+stop(_) ->
+	ok.
