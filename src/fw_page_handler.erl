fc2d566eba9792afe6ac10b2825dd1d02de59b5b
Loïc Hoguin <essen@ninenines.eu>
2013-04-04 22:24:11 +0200

    Add a mechanism for doing joins on the JSON data

--- src/fw_page_handler.erl
+++ src/fw_page_handler.erl
@@ -71,7 +71,13 @@ to_json(Req, State=#state{data=Data}) ->
 query_data(Rules, Req) ->
 	query_data(Rules, Req, []).
 query_data([], Req, Acc) ->
-	{ok, Acc, Req};
+	case get_links() of
+		[] ->
+			{ok, Acc, Req};
+		Links ->
+			[Data] = walk_list([Acc], Links, []),
+			{ok, Data, Req}
+	end;
 query_data([Rule|Tail], Req, Acc) ->
 	case query_field(Rule, Req) of
 		E = {error, _} ->
@@ -105,3 +111,66 @@ query_field({Name, {get_all, Bucket}}, Req) ->
 query_field({Name, {mfa, {M, F, A}}}, Req) ->
 	{ok, ValuesList} = apply(M, F, A),
 	{{Name, ValuesList}, Req}.
+
+get_links() ->
+	case application:get_env(farwest, links_file) of
+		{ok, {priv_dir, App, Path}} ->
+			{ok, Links} = code:priv_dir(App) ++ "/" ++ Path,
+			Links;
+		{ok, Filename} ->
+			{ok, Links} = file:consult(Filename),
+			Links;
+		_ ->
+			[]
+	end.
+
+walk_list([], _, Acc) ->
+	lists:reverse(Acc);
+walk_list([Value|Tail], Links, Acc) when is_list(Value) ->
+	case Value of
+		[Hd|_] when is_tuple(Hd) ->
+			Value2 = walk_array(Value, Links, []),
+			walk_list(Tail, Links, [Value2|Acc]);
+		_ ->
+			Value2 = walk_list(Value, Links, []),
+			walk_list(Tail, Links, [Value2|Acc])
+	end;
+walk_list([Value|Tail], Links, Acc) ->
+	walk_list(Tail, Links, [Value|Acc]).
+
+walk_array([], _, Acc) ->
+	lists:reverse(Acc);
+walk_array([{Key, []}|Tail], Links, Acc) ->
+	walk_array(Tail, Links, [{Key, []}|Acc]);
+walk_array([{Key, Value}|Tail], Links, Acc) when is_list(Value) ->
+	case Value of
+		[Hd|_] when is_tuple(Hd) ->
+			Value2 = walk_array(Value, Links, []),
+			walk_array(Tail, Links, [{Key, Value2}|Acc]);
+		_ ->
+			Value2 = walk_list(Value, Links, []),
+			walk_array(Tail, Links, [{Key, Value2}|Acc])
+	end;
+walk_array([{Key, Value}|Tail], Links, Acc) ->
+	Ignore = byte_size(Key) - 5,
+	case Key of
+		<< KeyPrefix:Ignore/binary, "_link" >> ->
+			case lists:keyfind(Key, 1, Links) of
+				false ->
+					walk_array(Tail, Links, [{Key, Value}|Acc]);
+				{_, Bucket, ValuePrefix} ->
+					PrefixSize = byte_size(ValuePrefix),
+					<< ValuePrefix:PrefixSize/binary, Suffix/bits >> = Value,
+					case fw_userdata_server:get_value(Bucket, Suffix) of
+						{ok, JSON} ->
+							DataKey = << KeyPrefix/binary, "_data" >>,
+							walk_array(Tail, Links, [
+								{DataKey, jsx:decode(JSON)},
+								{Key, Value}|Acc]);
+						_ ->
+							walk_array(Tail, Links, [{Key, Value}|Acc])
+					end
+			end;
+		_ ->
+			walk_array(Tail, Links, [{Key, Value}|Acc])
+	end.
